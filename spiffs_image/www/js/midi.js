aed32022ff1f005801deb78f22e81d52d1113a99
Robert Manzke <robert.manzke@fh-kiel.de>
2021-01-06 12:49:01 +0100

    first controllers working

--- spiffs_image/www/js/midi.js
+++ spiffs_image/www/js/midi.js
@@ -3,16 +3,16 @@
 if (navigator.requestMIDIAccess) {
     navigator.requestMIDIAccess({
         sysex: false
-    }).then( m =>{
+    }).then(m => {
             // get input devices and store in global object
             window.midiCtrl = {};
             window.midiCtrl.devices = m;
             // check if cookie for settings exist and select current item if device exists
             let actMidiInDevID = getCookie('midi-in');
             window.midiCtrl.actMidiInDevID = 0;
-            if(actMidiInDevID){
-                for(let i of midiCtrl.devices.inputs.values()){
-                    if(i.id == actMidiInDevID){
+            if (actMidiInDevID) {
+                for (let i of midiCtrl.devices.inputs.values()) {
+                    if (i.id == actMidiInDevID) {
                         window.midiCtrl.actMidiInDevID = actMidiInDevID;
                         window.midiCtrl.actMidiInDev = i;
                         break;
@@ -20,9 +20,9 @@ if (navigator.requestMIDIAccess) {
                 }
             }
             // not found?
-            if(window.midiCtrl.actMidiInDevID == 0){
+            if (window.midiCtrl.actMidiInDevID == 0) {
                 setCookie('midi-in', '0', 365);
-            }else{// otherwise register handler
+            } else {// otherwise register handler
                 bindMidiMethods();
                 window.midiCtrl.actMidiInDev.onmidimessage = processMidiEvent;
             }
@@ -35,7 +35,7 @@ if (navigator.requestMIDIAccess) {
 }
 
 // midi methods
-function bindMidiMethods(){
+function bindMidiMethods() {
     midiCtrl.midiLearn = (ch, el, target) => {
         console.log('Midi learn');
         console.log(ch);
@@ -45,27 +45,46 @@ function bindMidiMethods(){
     };
     midiCtrl.midiLearnOnMsg = (e) => {
         let m = midiCtrl.parseMidiEvent(e);
-        if(m){ // valid automatization ?
+        if (m) { // valid automatization ?
             console.log(midiCtrl.learn);
             midiCtrl.learn.t.style.color = 'green';
-            midiCtrl.midiCtrlMappings.push({filter: m, param: {ch: midiCtrl.learn.ch, el: midiCtrl.learn.el}});
+            midiCtrl.midiCtrlMappings.push({filter: m, param: {ch: midiCtrl.learn.ch, el: midiCtrl.learn.el}}); // do not confuse plugin ch with midi filter ch!
             console.log(midiCtrl.midiCtrlMappings);
-        }else{
+        } else {
             midiCtrl.learn.t.style.color = 'inherit';
         }
         window.midiCtrl.actMidiInDev.onmidimessage = midiCtrl.midiProcess;
         midiCtrl.learn = undefined;
     };
-    midiCtrl.midiProcess = () => {
+    midiCtrl.midiProcess = (e) => {
+        let m = midiCtrl.parseMidiEvent(e);
+        if (!m) return; // no valid automation event!
 
+        // check if anything needs to be controlled and do control
+        midiCtrl.midiCtrlMappings.forEach(el => {
+            if(m.type == el.filter.type && m.ch == el.filter.ch && m.key == el.filter.key){ // found mapping for this midi event!
+                console.log('Found mapping, executing REST call!');
+                let val = Math.trunc(m.val/16384 * (el.param.el.max - el.param.el.min) + el.param.el.min); // always 14 bit scaled midi
+                console.log('value: ' + val);
+                $.getq('myq',
+                    'api/v1/setPluginParam/' + el.param.ch,
+                    {
+                        id: el.param.el.id,
+                        current: val
+                    }
+                );
+            }
+        });
     };
     midiCtrl.midiCtrlMappings = [];
-    midiCtrl.isMidiLearn = false;
+    midiCtrl.learn = undefined; // this will contain the parameter to be learned if learning mode is active
     midiCtrl.parseMidiEvent = (e) => {
+        // return value is undefined if no valid midi event for automation
+        // otherwise {type, midiCh, key, value (always 14bit scaled)}
         let midiChannel = e.data[0] & 0x0F;
         switch (e.data[0] & 0xf0) {
             case 0x90:
-                if (e.data[2]!=0) {  // if velocity != 0, this is a note-on message
+                if (e.data[2] != 0) {  // if velocity != 0, this is a note-on message
                     console.log('Note on');
                     break;
                 }
@@ -74,11 +93,11 @@ function bindMidiMethods(){
                 console.log('Note off');
                 break;
             case 0xa0:
-                console.log('Poly Key Pressure value: ' );
+                console.log('Poly Key Pressure value: ');
                 break;
             case 0xB0:
                 console.log('Controller: ' + e.data[1] + ' value: ' + e.data[2]);
-                return {type: 'c', ch: midiChannel, key: e.data[1], val: e.data[2]};
+                return {type: 'c', ch: midiChannel, key: e.data[1], val: e.data[2]<<7};
             case 0xC0:
                 console.log('Program change');
                 break;
@@ -86,8 +105,8 @@ function bindMidiMethods(){
                 console.log('Channel pressure');
                 break;
             case 0xE0:
-                console.log('Pitch bend value: ' + (e.data[1] + (e.data[2]<<7)));
-                return {type: 'p', ch: midiChannel, val: (e.data[1] + (e.data[2]<<7))};
+                console.log('Pitch bend value: ' + (e.data[1] + (e.data[2] << 7)));
+                return {type: 'p', ch: midiChannel, key: 0, val: (e.data[1] + (e.data[2] << 7))};
             case 0xF0:
                 console.log('Sysx');
                 break;
@@ -100,13 +119,13 @@ function bindMidiMethods(){
 
 
 // process and list midi events
-function processMidiEvent( event ) {
+function processMidiEvent(event) {
     // parse event
     let midiChannel = event.data[0] & 0x0F;
     console.log('Midi Channel' + midiChannel);
     switch (event.data[0] & 0xf0) {
         case 0x90:
-            if (event.data[2]!=0) {  // if velocity != 0, this is a note-on message
+            if (event.data[2] != 0) {  // if velocity != 0, this is a note-on message
                 console.log('Note on');
                 break;
             }
@@ -115,7 +134,7 @@ function processMidiEvent( event ) {
             console.log('Note off');
             break;
         case 0xa0:
-            console.log('Poly Key Pressure value: ' );
+            console.log('Poly Key Pressure value: ');
             break;
         case 0xB0:
             console.log('Controller: ' + event.data[1] + ' value: ' + event.data[2]);
@@ -127,7 +146,7 @@ function processMidiEvent( event ) {
             console.log('Channel pressure');
             break;
         case 0xE0:
-            console.log('Pitch bend value: ' + (event.data[1] + (event.data[2]<<7)));
+            console.log('Pitch bend value: ' + (event.data[1] + (event.data[2] << 7)));
             break;
         case 0xF0:
             console.log('Sysx');
